420106ffd2705e75931ce02456de0562a754bc72
Derek <derekjou0@gmail.com>
2019-07-18 13:48:37 -0400

    display list

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<p>React yolo!</p>, document.getElementById('root'))
+let dummyData = ["cry", "sleep", "repeat"];
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+      return (
+        <li><button>X</button> {this.props.task}</li>
+      );
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        {dummyData.map(task => (
+          <Todo task={task} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+class InputLine extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+ReactDOM.render(<TodoList />, document.getElementById("root"));
